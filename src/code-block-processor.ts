47a9cd6e0944b3d6c235ec81efa57e8876be7374
0918nobita <nobita.0918@gmail.com>
2023-11-29 18:03:28 +0900

    :recycle: Refactor around code block processor and settings

---
+++ src/code-block-processor.ts
@@ -0,0 +1,65 @@
+import { pinyin } from 'pinyin-pro';
+
+import type { Settings } from './settings';
+
+import * as styles from './style.css';
+
+export const codeBlockProcessor = async (
+    source: string,
+    element: HTMLElement,
+    settings: Settings,
+) => {
+    const gap = 2;
+
+    const pinyinData = pinyin(source, { type: 'all' });
+
+    await new Promise((resolve) => {
+        setTimeout(resolve, 0);
+    });
+
+    const container = element.createDiv({
+        cls: styles.container,
+    });
+
+    const pinyinLine = container.createDiv({
+        cls: settings.alwaysDisplayPinyin
+            ? styles.pinyinLineBase
+            : styles.pinyinLineDisplayedOnHover,
+    });
+
+    const chineseCharLine = container.createDiv({
+        cls: styles.chineseCharLine,
+    });
+
+    for (const pinyinDatum of pinyinData) {
+        const pinyinSpan = pinyinLine.createSpan({
+            text: pinyinDatum.pinyin,
+            cls: styles.pinyinSpan,
+        });
+        const pinyinWidth = pinyinSpan.getBoundingClientRect().width;
+
+        const chineseCharSpan = chineseCharLine.createSpan({
+            text: pinyinDatum.origin,
+            cls: styles.chineseCharSpan,
+        });
+        const chineseCharWidth = chineseCharSpan.getBoundingClientRect().width;
+
+        const { pinyinPadding, chineseCharPadding } =
+            pinyinWidth >= chineseCharWidth
+                ? {
+                      pinyinPadding: gap,
+                      chineseCharPadding:
+                          (pinyinWidth - chineseCharWidth) / 2 + gap,
+                  }
+                : {
+                      pinyinPadding: (chineseCharWidth - pinyinWidth) / 2 + gap,
+                      chineseCharPadding: gap,
+                  };
+
+        pinyinSpan.style.paddingLeft = `${pinyinPadding}px`;
+        pinyinSpan.style.paddingRight = `${pinyinPadding}px`;
+
+        chineseCharSpan.style.paddingLeft = `${chineseCharPadding}px`;
+        chineseCharSpan.style.paddingRight = `${chineseCharPadding}px`;
+    }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,84 +1,16 @@
-import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { pinyin } from 'pinyin-pro';
+import * as Obsidian from 'obsidian';
 
-import * as styles from './style.css';
+import { codeBlockProcessor } from './code-block-processor';
+import { type Settings, defaultSettings } from './settings';
 
-interface ZhongwenBlockPluginSettings {
-    alwaysDisplayPinyin: boolean;
-}
-
-const defaultSettings: ZhongwenBlockPluginSettings = {
-    alwaysDisplayPinyin: false,
-};
-
-const codeBlockProcessor = async (
-    source: string,
-    element: HTMLElement,
-    settings: ZhongwenBlockPluginSettings,
-) => {
-    const gap = 2;
-
-    const pinyinData = pinyin(source, { type: 'all' });
-
-    await new Promise((resolve) => {
-        setTimeout(resolve, 0);
-    });
-
-    const container = element.createDiv({
-        cls: styles.container,
-    });
-
-    const pinyinLine = container.createDiv({
-        cls: settings.alwaysDisplayPinyin
-            ? styles.pinyinLineBase
-            : styles.pinyinLineDisplayedOnHover,
-    });
-
-    const chineseCharLine = container.createDiv({
-        cls: styles.chineseCharLine,
-    });
-
-    for (const pinyinDatum of pinyinData) {
-        const pinyinSpan = pinyinLine.createSpan({
-            text: pinyinDatum.pinyin,
-            cls: styles.pinyinSpan,
-        });
-        const pinyinWidth = pinyinSpan.getBoundingClientRect().width;
-
-        const chineseCharSpan = chineseCharLine.createSpan({
-            text: pinyinDatum.origin,
-            cls: styles.chineseCharSpan,
-        });
-        const chineseCharWidth = chineseCharSpan.getBoundingClientRect().width;
-
-        const { pinyinPadding, chineseCharPadding } =
-            pinyinWidth >= chineseCharWidth
-                ? {
-                      pinyinPadding: gap,
-                      chineseCharPadding:
-                          (pinyinWidth - chineseCharWidth) / 2 + gap,
-                  }
-                : {
-                      pinyinPadding: (chineseCharWidth - pinyinWidth) / 2 + gap,
-                      chineseCharPadding: gap,
-                  };
-
-        pinyinSpan.style.paddingLeft = `${pinyinPadding}px`;
-        pinyinSpan.style.paddingRight = `${pinyinPadding}px`;
-
-        chineseCharSpan.style.paddingLeft = `${chineseCharPadding}px`;
-        chineseCharSpan.style.paddingRight = `${chineseCharPadding}px`;
-    }
-};
-
-export default class ZhongwenBlockPlugin extends Plugin {
-    settings!: ZhongwenBlockPluginSettings;
+export default class ZhongwenBlockPlugin extends Obsidian.Plugin {
+    settings!: Settings;
 
     async onload() {
         this.settings = Object.assign(
             {},
             defaultSettings,
-            (await this.loadData()) as ZhongwenBlockPluginSettings,
+            (await this.loadData()) as Settings,
         );
         this.addSettingTab(new ZhongwenBlockPluginSettingTab(this.app, this));
         this.registerMarkdownCodeBlockProcessor('zh-cn', (source, element) =>
@@ -91,10 +23,10 @@ export default class ZhongwenBlockPlugin extends Plugin {
     }
 }
 
-class ZhongwenBlockPluginSettingTab extends PluginSettingTab {
+class ZhongwenBlockPluginSettingTab extends Obsidian.PluginSettingTab {
     plugin: ZhongwenBlockPlugin;
 
-    constructor(app: App, plugin: ZhongwenBlockPlugin) {
+    constructor(app: Obsidian.App, plugin: ZhongwenBlockPlugin) {
         super(app, plugin);
         this.plugin = plugin;
     }
@@ -104,7 +36,7 @@ class ZhongwenBlockPluginSettingTab extends PluginSettingTab {
 
         containerEl.empty();
 
-        new Setting(containerEl)
+        new Obsidian.Setting(containerEl)
             .setName('Always display pinyin')
             .addToggle((toggle) =>
                 toggle
