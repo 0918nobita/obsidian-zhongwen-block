b704b5240fdfe5ce6af91cdb522c122570951529
Kodai Matsumoto <nobita.0918@gmail.com>
2024-05-11 21:46:47 +0900

    Svelte を用いてコードブロックプロセッサを実装し直した

--- src/code-block-processor/memo.ts
+++
@@ -1,8 +0,0 @@
-export interface ZhCharBlockLayout {
-    zhChar: string;
-    zhCharWidth: number;
-    pinyin: string;
-    pinyinWidth: number;
-}
-
-export type LayoutMemo = Map<string, ZhCharBlockLayout[]>;
