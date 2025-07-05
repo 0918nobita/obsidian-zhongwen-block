7e1e6b349350d95d0179d93ac3122937d3947040
Kodai Matsumoto <nobita.0918@gmail.com>
2025-07-05 16:45:35 +0900

    wip

--- .prettierrc.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('prettier').Config} */
-export default {
-    plugins: ['prettier-plugin-svelte'],
-    singleQuote: true,
-    semi: true,
-};
---
+++ src/components/TextLine.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import type { Snippet } from 'svelte';
+
+interface Props {
+    children: Snippet;
+}
+
+const { children }: Props = $props();
+</script>
+
+<div>
+    {@render children()}
+</div>
+
+<style scoped>
+    div {
+        display: inline-flex;
+        align-items: flex-end;
+        flex-wrap: wrap;
+    }
+</style>
